9a781f031854a2450f11986aea9fdf0f5653cb0c
susanirago22 <susaniragoorani@gmail.com>
2022-11-24 12:20:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.querySelector("#main").remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id","avocado");
+newHeader.textContent = "I love avocado";
